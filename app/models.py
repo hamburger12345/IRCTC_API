26f0e42bb80adc8ddd01701f3303a83bec4b194b
hamburger12345 <86918794+hamburger12345@users.noreply.github.com>
2024-04-08 01:26:38 +0530

    Update models.py

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+class Train(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(100), nullable=False)
+    destination = db.Column(db.String(100), nullable=False)
+    total_seats = db.Column(db.Integer, nullable=False)
+    available_seats = db.Column(db.Integer, nullable=False)
